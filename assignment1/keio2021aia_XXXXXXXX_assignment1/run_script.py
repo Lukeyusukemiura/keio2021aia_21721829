e324163f16670aee0016a30af65a167eef8ad9e6
yusuke <hongkong34ctower2@icloud.com>
2021-06-03 20:16:00 +0900

    init_runscript

--- assignment1/keio2021aia_XXXXXXXX_assignment1/run_script.py
+++ assignment1/keio2021aia_XXXXXXXX_assignment1/run_script.py
@@ -1,28 +1,33 @@
-
+##
 from sonar.sonar_data import * 
 from sonar.sonar_model import * 
 from sonar.sonar_trainer import * 
 from cats.cat_data import *
 from cats.cat_model import *
 from cats.cat_trainer import *
+##
 
-
-
+##
 def main():
-    data = Sonar_Data('','sonar_data.pkl')
-    model = Sonar_Model()
-    trainer = Sonar_Trainer(data.data,model)
-    costs, accuracies = trainer.train(0.5,100)
-    model.save_model()
+   
+  Data=Sonar_Data()
+ 
+  model =Sonar_Model(dimension=len([next(iter(Data))][0][0][0]), activation=perceptron)
 
-    data = Cat_Data('','cat_data.pkl')
-    model = Cat_Model()
-    trainer = Cat_Trainer(data,model)
-    costs, accuracies = trainer.train(0.5,100)
-    model.save_model()
+  trainer = Sonar_Trainer(next(iter(Data)), model)
 
+  costs, accuracies =trainer.train(0.1, 500)
+  
+  model.save_model()
+ 
+   
+  Data=Cat_Data()
+  model =Cat_Model(dimension=len([next(iter(Data))][0][0][0]), activation=sigmoid)
+  trainer = Cat_Trainer(Data, model)
+  costs, accuracies =trainer.train(0.1, 80)
+  model.save_model()
+##
 
-
+##
 if __name__ == "__main__":
     main()
-
